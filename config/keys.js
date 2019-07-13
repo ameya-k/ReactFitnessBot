de050488a89cc1c629a5670417007ce8425b01a2
ameyakulkarni <abkulkar@usc.edu>
2019-07-13 13:35:07 +0530

    implemented async await paradigm for calling API

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports={
+    googleProjectId:"react-fitness-bot-dxhskt",
+    sessionId:"react-ftbot-session",
+    dialogFlowLanguage:'en-US'
+
+}
--- routes/DialogFlowRoutes.js
+++ routes/DialogFlowRoutes.js
@@ -1,11 +1,37 @@
+
+const  df=require('dialogflow');
+const conf=require('../config/keys');
+
+const sessionClient=new df.SessionsClient();
+
+const seesionPath=sessionClient.sessionPath(conf.googleProjectId,conf.sessionId);
+
 module.exports=app=>{
 
     app.get('/',(req,res)=>{
         res.send({'message':'Hello from FitBot chatbot!'});
     });
 
-    app.post('/api/text_bot',(req,res)=>{
-        res.send({'message':'Responds to text query'});
+    app.post('/api/text_bot',async (req,res)=>{
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    // The query to send to the dialogflow agent
+                    text: req.body.text,
+                    // The language used by the client (en-US)
+                    languageCode: conf.dialogFlowLanguage,
+                },
+            },
+        };
+
+        let responses=await sessionClient.detectIntent(request);
+
+
+
+
+        res.send(responses[0].queryResult);
     });
 
     app.post('/api/event_bot',(req,res)=>{
