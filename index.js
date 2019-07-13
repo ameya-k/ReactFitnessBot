0dcf40642fa65f8dd8b7c8dd4e243cde9741d0b5
ameyakulkarni <abkulkar@usc.edu>
2019-07-13 12:58:22 +0530

    setting up heroku and nodemon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 const PORT_NUMBER=process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-    res.send({'message':'Hello from FitBot!'});
+    res.send({'message':'Hello from FitBot chatbot!'});
 });
 
 
