fc059e978492123a38c5ebf140ce86e9713ee1f7
ameyakulkarni <abkulkar@usc.edu>
2019-07-13 12:41:50 +0530

    setting up heroku

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
 const app=express();
 
+const PORT_NUMBER=process.env.PORT || 5000;
+
 app.get('/',(req,res)=>{
     res.send({'message':'Hello from FitBot!'});
 });
 
 
-app.listen(5000);
+
+app.listen(PORT_NUMBER);
