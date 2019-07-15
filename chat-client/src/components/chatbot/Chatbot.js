2ead7eb74a8b84d9dbb778395e47b2ad22ce8783
ameyakulkarni <abkulkar@usc.edu>
2019-07-15 15:22:09 +0530

    user input implemented

--- chat-client/src/components/chatbot/Chatbot.js
+++ chat-client/src/components/chatbot/Chatbot.js
@@ -7,6 +7,11 @@ class Chatbot extends Component {
         messages:[]
     };
 
+    constructor(props){
+        super(props);
+        this.inputChange=this.inputChange.bind(this);
+    }
+
     async text_bot_query(text){
 
         let says={
@@ -18,7 +23,7 @@ class Chatbot extends Component {
             }
         }
         this.setState({messages:[...this.state.messages,says]});
-        const res=await axios.post('/api/text_bot/',{text});
+        const res=await axios.post('http://localhost:5000/api/text_bot/',{text});
         for (let msg of res.data.fulfillmentMessages){
             says= {
                 speaks: 'FitBot',
@@ -48,6 +53,8 @@ class Chatbot extends Component {
         this.event_bot_query('Welcome');
     }
 
+
+
     showMessages(messages){
         if(messages){
             return messages.map((message,i)=>{
@@ -59,13 +66,20 @@ class Chatbot extends Component {
         }
     }
 
+    inputChange(e){
+        if(e.key==='Enter'){
+            this.text_bot_query(e.target.value);
+            e.target.value='';
+        }
+    }
+
     render(){
         return(
             <div className="container" style={{height:'400',width:'400',float:'center'}}>
                 <div id='chatbot' style={{height:'100%',width:'100%',overflow:'auto'}}>
                     <h2>Chatbot</h2>
                     {this.showMessages(this.state.messages)}
-                    <input type='text'/>
+                    <input type='text' onKeyPress={this.inputChange}/>
                 </div>
             </div>
         )
